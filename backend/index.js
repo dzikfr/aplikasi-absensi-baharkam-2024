c6469d9b6cece3f47ae00b9487b5eae002930c2d
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-11-04 21:52:06 +0700

    clear be

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const {config} = require('dotenv');
 const cors = require('cors');
 const connectDb = require('./config/db.js');
-const adminRouter = require('./routes/adminRouter.js');
-const employeeRouter = require('./routes/employeeRouter.js');
-const attendanceRouter = require('./routes/attendanceRouter.js');
 
 //init express
 const app = express();
@@ -21,18 +18,6 @@ config();
 //connect db
 //./config/db.js
 connectDb();
-
-
-//api
-app.use('/api', adminRouter);
-app.use('/api', employeeRouter);
-app.use('/api', attendanceRouter);
-
-
-//get no routes to asset
-app.use('/assets/data', (req, res, next) => {
-    res.status(403).send('Access Forbidden');
-});
 
 
 //port listen server app
