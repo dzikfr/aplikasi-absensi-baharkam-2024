2dcb22d8e5de0ab85ce8dde3d75cf40bdabb9694
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-11-19 10:02:08 +0700

    try fix be for deploy

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('database connected');
-    } catch (error) {
-        console.error('Error db config: ', error);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Error db config: ", error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDb;
